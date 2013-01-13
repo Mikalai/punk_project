6e0da734f5cae0ef81ef2bc8fdaddec5ed001df0
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-13 23:01:21 +0300

    terrain module files
    files are empty

---
+++ source/virtual/terrain/module.h
@@ -0,0 +1,12 @@
+#ifndef _H_PUNK_VIRTUAL_TERRAIN_MODULE
+#define _H_PUNK_VIRTUAL_TERRAIN_MODULE
+
+#include "terrain.h"
+#include "terrain_cell.h"
+#include "terrain_data.h"
+#include "terrain_manager.h"
+#include "terrain_observer.h"
+#include "terrain_streamer.h"
+#include "terrain_view.h"
+
+#endif	//	_H_PUNK_VIRTUAL_TERRAIN_MODULE
---
+++ source/virtual/terrain/terrain_data.h
@@ -0,0 +1,17 @@
+#ifndef _H_PUNK_VIRTUAL_TERRAIN_DATA
+#define _H_PUNK_VIRTUAL_TERRAIN_DATA
+
+#include "../../config.h"
+#include "../../string/string.h"
+
+namespace Virtual
+{
+	class PUNK_ENGINE TerrainData
+	{
+	public:
+	private:		
+		float* m_data;
+	};
+}
+
+#endif	//	_H_PUNK_VIRTUAL_TERRAIN_DATA
