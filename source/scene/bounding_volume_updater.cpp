e34e3fccf9c05185f9afc3699edc4b0aa25f63ed
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-13 23:15:27 +0300

    bounding volume updater added to the scene graph

---
+++ source/scene/bounding_volume_updater.cpp
@@ -0,0 +1,250 @@
+﻿#include "bounding_volume_updater.h"
+#include "../virtual/data/data.h"
+#include "../virtual/skinning/skinning.h"
+#include "../virtual/animation/anim.h"
+
+#include "scene_graph.h"
+#include "../opengl/renderable/renderable_data.h"
+
+namespace Scene
+{
+
+	bool BoundingVolumeUpdater::Visit(Scene::CameraNode* node)
+	{
+		m_states.Push();
+
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+				child->Apply(this);
+		}
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::StaticMeshNode* node)
+	{		
+		System::Proxy<OpenGL::StaticMesh> mesh = OpenGL::StaticMeshManager::Instance()->Load(node->GetStorageName());	
+		node->SetBoundingSphere(m_states.CurrentState()->Get().m_local * mesh->GetBoundingSphere());
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::SkinMeshNode* node)
+	{				
+		System::Proxy<OpenGL::SkinMesh> mesh = OpenGL::SkinMeshManager::Instance()->Load(node->GetStorageName());	
+		node->SetBoundingSphere(m_states.CurrentState()->Get().m_local * mesh->GetBoundingSphere());
+		return true;
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::ArmatureNode* node)
+	{				
+		m_states.Push();	
+		System::Proxy<Virtual::Armature> armature = Virtual::ArmatureManager::Instance()->Load(node->GetStorageName());
+
+		m_states.CurrentState()->Get().m_armature = armature;
+		m_states.CurrentState()->Get().m_armature_world = m_states.CurrentState()->Get().m_local;
+
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		m_states.Pop();
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::BoneNode* node)
+	{
+		m_states.Push();
+		System::Proxy<Virtual::Armature> armature = m_states.CurrentState()->Get().m_armature;
+		if (!armature.IsValid())
+			return (out_error() << "It is impossible to process bone node without valid armature" << std::endl, false);
+
+		Virtual::Bone* bone = armature->GetBoneByName(node->GetName());
+		if (!bone)
+			return (out_error() << "Bone " << node->GetName() << std::endl, false);
+
+		m_states.CurrentState()->Get().m_local *= bone->GetAnimatedGlobalMatrix();
+
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		m_states.Pop();
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::LightNode* node)
+	{
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::MaterialNode* node)
+	{
+		m_states.Push();		
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		m_states.Pop();
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::Node* node)
+	{
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		return true;
+	}
+	bool BoundingVolumeUpdater::Visit(Scene::TransformNode* node)
+	{
+		m_states.Push();
+		m_states.CurrentState()->Get().m_local *= node->GetLocalMatrix();
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		m_states.Pop();
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::LocationIndoorNode* node)
+	{
+		m_states.Push();
+		m_states.CurrentState()->Get().m_local *= node->GetLocalMatrix();
+		bool was = false;
+		for each (System::Proxy<Scene::Node> child in *node)
+		{
+			if (child.IsValid())
+			{
+				if (!child->Apply(this))
+					return false;
+				if (!was)
+				{
+					node->SetBoundingSphere(child->GetBoundingSphere());
+					was = true;
+				}
+				else
+				{
+					node->SetBoundingSphere(node->GetBoundingSphere() + child->GetBoundingSphere());
+				}
+			}
+
+		}
+		m_states.Pop();
+		return true;
+	}
+
+	bool BoundingVolumeUpdater::Visit(Scene::PortalNode* node)
+	{
+		return true;
+	}
+
+	void BoundingVolumeUpdater::Update()
+	{
+		if (m_scene.IsValid())
+			m_scene->GetRootNode()->Apply(this);
+	}
+
+	void BoundingVolumeUpdater::SetScene(System::Proxy<Scene::SceneGraph> scene)
+	{
+		m_scene = scene;
+	}
+
+
+}
---
+++ source/scene/bounding_volume_updater.h
@@ -0,0 +1,49 @@
+#ifndef _H_PUNK_SCENE_BOUNDING_VOLUME_UPDATER
+#define _H_PUNK_SCENE_BOUNDING_VOLUME_UPDATER
+
+#include "../config.h"
+#include "visitor.h"
+#include "../system/smart_pointers/proxy.h"
+#include "../math/bounding_shere.h"
+#include "../system/state_manager.h"
+
+namespace Virtual { class Armature; }
+
+namespace Scene
+{	
+	class SceneGraph;
+
+	class PUNK_ENGINE BoundingVolumeUpdater : public AbstractVisitor
+	{
+	public:
+		virtual bool Visit(CameraNode* node);
+		virtual bool Visit(StaticMeshNode* node);
+		virtual bool Visit(SkinMeshNode* node);
+		virtual bool Visit(ArmatureNode* node);
+		virtual bool Visit(LightNode* node);
+		virtual bool Visit(MaterialNode* node);
+		virtual bool Visit(Node* node);
+		virtual bool Visit(TransformNode* node);
+		virtual bool Visit(LocationIndoorNode* node);		
+		virtual bool Visit(PortalNode* node);
+		virtual bool Visit(BoneNode* node);
+
+		void SetScene(System::Proxy<SceneGraph> value);
+		void Update();
+
+	private:
+
+		struct BoundingVolumeState
+		{
+			Math::mat4 m_local;
+			Math::mat4 m_armature_world;
+			Math::BoundingSphere m_sphere;
+			System::Proxy<Virtual::Armature> m_armature;			
+		};
+
+		System::StateManager<BoundingVolumeState> m_states;
+		System::Proxy<SceneGraph> m_scene;
+	};
+}
+
+#endif	//	_H_PUNK_SCENE_BOUNDING_VOLUME_UPDATER
