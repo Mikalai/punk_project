79f83d161ce4a3a002773a5fe7e96a4a517426aa
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-14 20:55:17 +0300

    thread class for windows added

---
+++ source/system/concurrency/win32/thread_win32.h
@@ -0,0 +1,15 @@
+#ifndef _H_PUNK_THREAD_WIN32
+#define _H_PUNK_THREAD_WIN32
+
+#include "../../../config.h"
+
+namespace System
+{
+	class PUNK_ENGINE Thread
+	{
+	public:
+	private:
+	};
+}
+
+#endif
