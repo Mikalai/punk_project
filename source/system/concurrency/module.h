83a0cd006b764cb25761e0bf77622ea44ebbefa4
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-15 22:38:10 +0300

    interface created for concurrency module

---
+++ source/system/concurrency/module.h
@@ -0,0 +1,9 @@
+#ifndef _H_SYSTEM_CONCURRENCY_MODULE
+#define _H_SYSTEM_CONCURRENCY_MODULE
+
+#include "thread.h"
+#include "thread_pool.h"
+#include "semaphore.h"
+#include "thread_mutex.h"
+
+#endif	//	_H_SYSTEM_CONCURRENCY_MODULE
