a0bf9231496c81ed314c758842f6ba95d014b2e8
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-15 22:40:46 +0300

    base type typedefs added to system

---
+++ source/system/types/base_types.h
@@ -0,0 +1,12 @@
+#ifndef _H_PUNK_SYSTEM_BASE_TYPES
+#define _H_PUNK_SYSTEM_BASE_TYPES
+
+#ifdef _WIN32
+#include "win32/base_types_win32.h"
+#endif
+
+#ifdef __linux__
+#include ""
+#endif
+
+#endif	//	_H_PUNK_SYSTEM_BASE_TYPES
---
+++ source/system/types/win32/base_types_win32.h
@@ -0,0 +1,19 @@
+#ifndef _H_PUNK_SYSTEM_BASE_TYPES_WIN32
+#define _H_PUNK_SYSTEM_BASE_TYPES_WIN32
+
+namespace System
+{	
+	typedef unsigned __int8 uint8;
+	typedef uint8 byte;
+	typedef signed __int8 int8;
+	typedef unsigned __int16 uint16;
+	typedef uint16 word;
+	typedef signed __int16 int16;
+	typedef unsigned __int32 uint32;
+	typedef uint32 dword;
+	typedef signed __int32 int32;
+	typedef unsigned __int64 uint64;
+	typedef signed __int64 int64;
+}
+
+#endif //	_H_PUNK_SYSTEM_BASE_TYPES_WIN32
