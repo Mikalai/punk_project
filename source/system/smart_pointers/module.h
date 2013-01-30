4859719b39ccc480e2815860b5ae8a097bb2e886
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:22:46 +0300

    System::Proxy<> modified for maybe better concurrency behavior

---
+++ source/system/smart_pointers/module.h
@@ -0,0 +1,8 @@
+#ifndef _H_PUNK_SYSTEM_SMART_POINTERS
+#define _H_PUNK_SYSTEM_SMART_POINTERS
+
+#include "handle.h"
+#include "proxy.h"
+#include "ref_count.h"
+
+#endif	//	_H_PUNK_SYSTEM_SMART_POINTERS
