97e58764cbea9f0602856aa72169ca2f479bef5d
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:23:45 +0300

    Image got error submodule

---
+++ source/images/error/image_exceptions.h
@@ -0,0 +1,16 @@
+#ifndef _H_PUNK_IMAGE_EXCEPTIONS
+#define _H_PUNK_IMAGE_EXCEPTIONS
+
+#include "../../system/errors/module.h"
+
+namespace ImageModule
+{
+	class PUNK_ENGINE ImageException : public System::PunkException
+	{
+	public:
+		ImageException() : System::PunkException(L"ImageException") {}
+		ImageException(const System::string& msg) : System::PunkException(L"ImageException: " + msg) {}
+	};
+}
+
+#endif	//	_H_PUNK_IMAGE_EXCEPTIONS
---
+++ source/images/error/module.h
@@ -0,0 +1,6 @@
+#ifndef _H_PUNK_IMAGE_ERROR_MODULE
+#define _H_PUNK_IMAGE_ERROR_MODULE
+
+#include "image_exceptions.h"
+
+#endif	//	_H_PUNK_IMAGE_ERROR_MODULE
