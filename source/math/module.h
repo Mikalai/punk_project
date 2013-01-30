90d510026a2f230df2d464b971545f8f7b6460e8
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:23:25 +0300

    Math got module header

---
+++ source/math/module.h
@@ -0,0 +1,28 @@
+#ifndef _H_PUNK_MATH_MODULE
+#define _H_PUNK_MATH_MODULE
+
+#include "vec3.h"
+#include "vec2.h"
+#include "vec4.h"
+#include "mat2.h"
+#include "mat3.h"
+#include "mat4.h"
+#include "smart_matrix.h"
+#include "plane.h"
+#include "line3d.h"
+#include "line2d.h"
+#include "triangle2d.h"
+#include "triangle3d.h"
+#include "aabb.h"
+#include "low_level_math.h"
+#include "interpolation.h"
+#include "complex.h"
+#include "frustum.h"
+#include "matrix.h"
+#include "random.h"
+#include "noise.h"
+#include "constants.h"
+#include "oct_tree.h"
+#include "helper.h"
+
+#endif
