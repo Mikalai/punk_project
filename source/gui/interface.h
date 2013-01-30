81a336b79fdf6f13a3d77dc2ae14a46bca87391e
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:23:57 +0300

    GUI got module and interface headers

---
+++ source/gui/interface.h
@@ -0,0 +1,11 @@
+#ifndef _H_PUNK_GUI_INTERFACE
+#define _H_PUNK_GUI_INTERFACE
+
+namespace GUI
+{
+	class Widget;
+	class Manager;
+	class IGUIRender;
+}
+
+#endif	//	_H_PUNK_GUI_INTERFACE
---
+++ source/gui/module.h
@@ -0,0 +1,8 @@
+#ifndef _H_PUNK_GUI_MODULE
+#define _H_PUNK_GUI_MODULE
+
+#include "gui_render.h"
+#include "manager.h"
+#include "widget.h"
+
+#endif	//	_H_PUNK_GUI_MODULE
