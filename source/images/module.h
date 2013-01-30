e212dd6b7687c66000d9a3c12bff5508776576ec
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:23:35 +0300

    Image module got module header

---
+++ source/images/module.h
@@ -0,0 +1,12 @@
+#ifndef _H_PUNK_IMAGES_MODULE
+#define _H_PUNK_IMAGES_MODULE
+
+#include "../config.h"
+#include "internal_images\rgba_image.h"
+#include "internal_images\rgb_image.h"
+#include "internal_images\gray_image.h"
+#include "internal_images\image.h"
+#include "import_export\importer.h"
+#include "import_export\exporter.h"
+
+#endif	//	_H_PUNK_IMAGES_MODULE
