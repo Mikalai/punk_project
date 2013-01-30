c10f3622a0064a0303723a146e8387a4e388abcf
Mikalai Abramau <nickolaib2004@gmail.com>
2013-01-30 22:23:12 +0300

    Scene got interface

---
+++ source/scene/interface.h
@@ -0,0 +1,27 @@
+#ifndef _H_PUNK_SCENE_INTERFACE
+#define _H_PUNK_SCENE_INTERFACE
+
+#include "visitor.h"
+
+namespace Scene
+{
+	class ArmatureNode;
+	class BoneNode;
+	class BoundingVolumeUpdater;
+	class CameraNode;
+	class DefaultVisitor;
+	class GeometryNode;
+	class LightNode;
+	class LocationIndoorNode;
+	class MaterialNode;
+	class Node;
+	class PointLightNode;
+	class PortalNode;
+	class SceneGraph;
+	class SkinMeshNode;
+	class StaticMeshNode;
+	class TerrainNode;
+	class TransformNode;
+}
+
+#endif	//	_H_PUNK_SCENE_INTERFACE
---
+++ source/scene/module.h
@@ -0,0 +1,24 @@
+#ifndef _H_PUNK_SCENE_MODULE
+#define _H_PUNK_SCENE_MODULE
+
+#include "debug/texture_view_node.h"
+#include "armature_node.h"
+#include "bone_node.h"
+#include "bounding_volume_updater.h"
+#include "camera_node.h"
+#include "default_visitor.h"
+#include "geometry_node.h"
+#include "light_node.h"
+#include "location_indoor.h"
+#include "material_node.h"
+#include "node.h"
+#include "point_light_node.h"
+#include "portal_node.h"
+#include "scene_graph.h"
+#include "skin_mesh_node.h"
+#include "static_mesh_node.h"
+#include "terrain_node.h"
+#include "transform_node.h"
+#include "visitor.h"
+
+#endif	//	_H_PUNK_SCENE_MODULE
