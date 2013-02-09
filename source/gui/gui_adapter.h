958fb6cf34b74fe1d21c5a3e44e434321acc6a96
Mikalai Abramau <nickolaib2004@gmail.com>
2013-02-09 12:04:53 +0300

    gui got event adapter

---
+++ source/gui/gui_adapter.h
@@ -0,0 +1,18 @@
+#ifndef _H_PUNK_GUI_ADAPTER
+#define _H_PUNK_GUI_ADAPTER
+
+#include "events/interface.h"
+
+namespace GUI
+{
+	class Adapter
+	{
+	public:
+		virtual void OnSetFocusedEvent(SetFocusedEvent* event) = 0;
+		virtual void OnSetUnFocusedEvent(SetUnFocusedEvent* event) = 0;
+		virtual void OnMouseEnterEvent(MouseEnterEvent* event) = 0;
+		virtual void OnMouseLeaveEvent(MouseLeaveEvent* event) = 0;
+	};
+}
+
+#endif	//	_H_PUNK_GUI_ADAPTER
