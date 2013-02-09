8b21d90881c3db0ae36b86b78fb9bfd7881320bf
Mikalai Abramau <nickolaib2004@gmail.com>
2013-02-09 12:04:42 +0300

    gui events got their own interface

---
+++ source/gui/events/interface.h
@@ -0,0 +1,12 @@
+#ifndef _H_PUNK_GUI_EVENTS_INTERFACE
+#define _H_PUNK_GUI_EVENTS_INTERFACE
+
+namespace GUI
+{
+	class SetFocusedEvent;
+	class SetUnFocusedEvent;
+	class MouseEnterEvent;
+	class MouseLeaveEvent;
+}
+
+#endif	//	_H_PUNK_GUI_EVENTS_INTERFACE
