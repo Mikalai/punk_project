c9c772cd2765aa89d907de1b4388d67db089d90b
Mikalai Abramau <nickolaib2004@gmail.com>
2013-02-20 20:45:45 +0300

    Texture2D abstraction added to the GPU

---
+++ source/gpu/common/texture2d.h
@@ -0,0 +1,14 @@
+#ifndef _H_PUNK_GPU_TEXTURE_2D
+#define _H_PUNK_GPU_TEXTURE_2D
+
+#include "../gpu_config.h"
+namespace GPU
+{
+	class Texture2D
+	{
+	public:
+
+	};
+}
+
+#endif	//	_H_PUNK_GPU_TEXTURE_2D
