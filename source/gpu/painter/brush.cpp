ce0a48d5ff00cebaff1985cc665306472734fdda
Mikalai Abramau <nickolaib2004@gmail.com>
2013-02-20 20:44:49 +0300

    Brush class added to the painter

---
+++ source/gpu/painter/brush.cpp
@@ -0,0 +1,116 @@
+#include "../../math/mat3.h"
+#include "../../math/vec4.h"
+
+#include "brush.h"
+
+namespace GPU
+{
+	struct Brush::BrushImpl
+	{
+		Math::vec4 m_color;
+		Math::mat3 m_transform;
+		int m_style;
+		const OpenGL::Texture2D* m_texture;
+
+		void SetColor(const Math::vec4& value)
+		{
+			m_color = value;
+		}
+
+		const Math::vec4& GetColor() const
+		{
+			return m_color;
+		}
+
+		void SetMatrix(const Math::mat3& value)
+		{
+			m_transform = value;
+		}
+
+		const Math::mat3& GetMatrix() const
+		{
+			return m_transform;
+		}
+
+		void SetStyle(int value)
+		{
+			m_style = value;
+		}
+
+		int GetStyle() const
+		{
+			return m_style;
+		}
+
+		void SetTexture(const OpenGL::Texture2D* texture)
+		{
+			m_texture = texture;
+		}
+
+		const OpenGL::Texture2D* GetTexture() const
+		{
+			return m_texture;
+		}
+	};
+
+	Brush::Brush()
+		: impl(new BrushImpl)
+	{}
+
+	Brush::~Brush()
+	{
+		delete impl;
+		impl = nullptr;
+	}
+
+	Brush::Brush(const Brush& value)
+		: impl(new BrushImpl(*value.impl))
+	{}
+
+	Brush& Brush::operator= (const Brush& value)
+	{
+		Brush temp(value);
+		std::swap(temp.impl, impl);
+		return *this;
+	}
+
+	void Brush::SetColor(const Math::vec4& value)
+	{
+		impl->SetColor(value);
+	}
+
+	const Math::vec4& Brush::GetColor() const
+	{
+		return impl->GetColor();
+	}
+
+	void Brush::SetMatrix(const Math::mat3& value)
+	{
+		impl->SetMatrix(value);
+	}
+
+	const Math::mat3& Brush::GetMatrix() const
+	{
+		return impl->GetMatrix();
+	}
+
+	void Brush::SetStyle(int value)
+	{
+		impl->SetStyle(value);
+	}
+
+	int Brush::GetStyle() const
+	{
+		return impl->GetStyle();
+	}
+
+	void Brush::SetTexture(const OpenGL::Texture2D* texture)
+	{
+		impl->SetTexture(texture);
+	}
+
+	const OpenGL::Texture2D* Brush::GetTexture() const
+	{
+		return impl->GetTexture();
+	}
+}
--- source/gpu/painter/brush.h
+++ source/gpu/painter/brush.h
@@ -1,6 +1,8 @@
 #ifndef _H_PUNK_GPU_BRUSH
 #define _H_PUNK_GPU_BRUSH
 
+#include "../../config.h"
+
 namespace Math 
 {
 	class vec4;
@@ -12,10 +14,14 @@ namespace GPU
 {
 	namespace OpenGL { class Texture2D; }
 
-	class Brush
+	class PUNK_ENGINE Brush sealed
 	{
 	public:
 		Brush();		
+		Brush(const Brush& value);
+		Brush& operator = (const Brush& value);
+		~Brush();
+
 		void SetColor(const Math::vec4& value);
 		const Math::vec4& GetColor() const;
 		void SetMatrix(const Math::mat3& value);
@@ -24,6 +30,9 @@ namespace GPU
 		int GetStyle() const;
 		void SetTexture(const OpenGL::Texture2D* texture);
 		const OpenGL::Texture2D* GetTexture() const;
+
+		struct BrushImpl;
+		BrushImpl* impl;
 	};
 }
 
