f064b4ee8c5b400984007bc3b7f7dd1cedace346
Mikalaj Abramau <nickolaib2004@gmail.com>
2013-05-30 21:50:53 +0300

    constructor staff added

--- source/system/events/mouse_move_event.cpp
+++ source/system/events/mouse_move_event.cpp
@@ -5,6 +5,10 @@ namespace System
     MouseMoveEvent::MouseMoveEvent()
     {
         eventCode = EVENT_MOUSE_MOVE;
+		x = y = x_prev = y_prev = 0;
+		controlKey = leftButton = middleButton =
+				rightButton = shiftButton =
+				xbutton1 = xbutton2 = false;
     }
 
 	string MouseMoveEvent::ToString()
