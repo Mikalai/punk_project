2e4d2a582a21f4f08a37f336ff0b947fc28112d4
Mikalaj <nickolaib2004@gmail.com>
2013-06-26 22:35:39 +0300

    Virtual: Terrain mesh got System::Aspect

--- source/virtual/terrain/terrain_mesh.cpp
+++ source/virtual/terrain/terrain_mesh.cpp
@@ -1,13 +1,39 @@
 #include "terrain_mesh.h"
+#include "../data/static_geometry.h"
 
 namespace Virtual
 {
     TerrainMesh::TerrainMesh()
+        : m_geometry(nullptr)
     {
     }
 
     TerrainMesh::~TerrainMesh()
     {
-        delete m_geometry;
+    }
+
+    void TerrainMesh::SetGeometry(StaticGeometry* geom)
+    {
+        m_geometry = geom;
+    }
+
+    StaticGeometry* TerrainMesh::GetGeometry()
+    {
+        return m_geometry;
+    }
+
+    const StaticGeometry* TerrainMesh::GetGeometry() const
+    {
+        return m_geometry;
+    }
+
+    void TerrainMesh::SetStaticMeshFilename(const System::string& filename)
+    {
+        m_filename = filename;
+    }
+
+    const System::string TerrainMesh::GetStaticMeshFilename() const
+    {
+        return m_filename;
     }
 }
--- source/virtual/terrain/terrain_mesh.h
+++ source/virtual/terrain/terrain_mesh.h
@@ -2,18 +2,27 @@
 #define TERRAIN_MESH_H
 
 #include "../../system/object.h"
+#include "../../system/aop/aop.h"
 
 namespace Virtual
 {
     class StaticGeometry;
     class Material;
 
-    class PUNK_ENGINE_API TerrainMesh : public System::Object
+    class PUNK_ENGINE_API TerrainMesh : public System::Object, public System::Aspect<TerrainMesh*, System::string>
     {
     public:
         TerrainMesh();
         virtual ~TerrainMesh();
+
+        void SetGeometry(StaticGeometry* geom);
+        StaticGeometry* GetGeometry();
+        const StaticGeometry* GetGeometry() const;
+
+        void SetStaticMeshFilename(const System::string& filename);
+        const System::string GetStaticMeshFilename() const;
     private:
+        System::string m_filename;
         StaticGeometry* m_geometry;
     };
 }
