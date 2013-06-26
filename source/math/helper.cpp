49be58f9fcaaa77359c16fe73acb7a20154679f3
Mikalaj <nickolaib2004@gmail.com>
2013-06-26 22:52:25 +0300

    Template function now not template

--- source/math/helper.cpp
+++ source/math/helper.cpp
@@ -407,4 +407,61 @@ namespace Math
 		dir.Normalize();
 		return true;
 	}
+
+    void CalculateTBN(const vec3& p1, const vec3& p2, const vec3& p3,
+        const vec2& tex1, const vec2& tex2, const vec2& tex3,
+        vec3& tng, vec3& btn, vec3& nrm, float& mm)
+    {
+        nrm = CalculateNormal(p1, p2, p3);
+        Matrix<float> s(2,2);
+        s.At(0,0) = (tex2 - tex1)[0];
+        s.At(0,1) = (tex2 - tex1)[1];
+        s.At(1,0) = (tex3 - tex1)[0];
+        s.At(1,1) = (tex3 - tex1)[1];
+
+        Matrix<float> q(2, 3);
+        q.At(0,0) = (p2 - p1)[0];
+        q.At(0,1) = (p2 - p1)[1];
+        q.At(0,2) = (p2 - p1)[2];
+        q.At(1,0) = (p3 - p1)[0];
+        q.At(1,1) = (p3 - p1)[1];
+        q.At(1,2) = (p3 - p1)[2];
+
+        Matrix<float> tb = s.Inversed()*q;
+
+        tng[0] = tb.At(0,0);
+        tng[1] = tb.At(0,1);
+        tng[2] = tb.At(0,2);
+
+        btn[0] = tb.At(1,0);
+        btn[1] = tb.At(1,1);
+        btn[2] = tb.At(1,2);
+
+        if (btn.Length()==0 || tng.Length() == 0)
+        {
+            nrm.Normalize();
+            mm = 1;
+        }
+        else
+        {
+            //
+            //	gram-shmidt normalization
+            //
+            nrm.Normalize();
+            tng = (tng - tng.Dot(nrm)*nrm).Normalize();
+            btn = (btn - btn.Dot(nrm)*nrm - btn.Dot(tng)*tng).Normalize();
+
+            Matrix<float> m(3,3);
+            m.At(0,0) = tng[0]; m.At(0,1) = tng[1]; m.At(0,2) = tng[2];
+            m.At(1,0) = btn[0]; m.At(1,1) = btn[1]; m.At(1,2) = btn[2];
+            m.At(2,0) = nrm[0]; m.At(2,1) = nrm[1]; m.At(2,2) = nrm[2];
+
+            mm = m.Determinant();/**/
+        }
+    }
+
+    const vec3 CalculateNormal(const vec3& p1, const vec3& p2, const vec3& p3)
+    {
+        return ((p2-p1).Cross(p3-p1)).Normalized();
+    }
 }
--- source/math/helper.h
+++ source/math/helper.h
@@ -248,65 +248,11 @@ namespace Math
 	{
 		return x*(1-a)+y*a;
 	}
-	template<class T>
-	Vector3<T> CalculateNormal(const Vector3<T>& p1, const Vector3<T>& p2, const Vector3<T>& p3)
-	{
-		return ((p2-p1).Cross(p3-p1)).Normalize();
-	}
 
-	template<class T>
-	static void CalculateTBN(const Vector3<T>& p1, const Vector3<T>& p2, const Vector3<T>& p3,
-		const Vector2<T>& tex1, const Vector2<T>& tex2, const Vector2<T>& tex3,
-		Vector3<T>& tng, Vector3<T>& btn, Vector3<T>& nrm, T& mm)
-	{
-		nrm = CalculateNormal(p1, p2, p3);
-		Matrix<T> s(2,2);
-		s.At(0,0) = (tex2 - tex1)[0];
-		s.At(0,1) = (tex2 - tex1)[1];
-		s.At(1,0) = (tex3 - tex1)[0];
-		s.At(1,1) = (tex3 - tex1)[1];
-
-		Matrix<T> q(2, 3);
-		q.At(0,0) = (p2 - p1)[0];
-		q.At(0,1) = (p2 - p1)[1];
-		q.At(0,2) = (p2 - p1)[2];
-		q.At(1,0) = (p3 - p1)[0];
-		q.At(1,1) = (p3 - p1)[1];
-		q.At(1,2) = (p3 - p1)[2];
-
-		Matrix<T> tb = s.Inversed()*q;
-
-		tng[0] = tb.At(0,0);
-		tng[1] = tb.At(0,1);
-		tng[2] = tb.At(0,2);
-
-		btn[0] = tb.At(1,0);
-		btn[1] = tb.At(1,1);
-		btn[2] = tb.At(1,2);
-
-		if (btn.Length()==0 || tng.Length() == 0)
-		{
-			nrm.Normalize();
-			mm = 1;
-		}
-		else
-		{
-			//
-			//	gram-shmidt normalization
-			//
-			nrm.Normalize();
-			tng = (tng - tng.Dot(nrm)*nrm).Normalize();
-			btn = (btn - btn.Dot(nrm)*nrm - btn.Dot(tng)*tng).Normalize();
-
-			Matrix<T> m(3,3);
-			m.At(0,0) = tng[0]; m.At(0,1) = tng[1]; m.At(0,2) = tng[2];
-			m.At(1,0) = btn[0]; m.At(1,1) = btn[1]; m.At(1,2) = btn[2];
-			m.At(2,0) = nrm[0]; m.At(2,1) = nrm[1]; m.At(2,2) = nrm[2];
-
-			mm = m.Determinant();/**/
-		}
-	}
-
+    PUNK_ENGINE_API const vec3 CalculateNormal(const vec3& p1, const vec3& p2, const vec3& p3);
+    PUNK_ENGINE_API void CalculateTBN(const vec3& p1, const vec3& p2, const vec3& p3,
+        const vec2& tex1, const vec2& tex2, const vec2& tex3,
+        vec3& tng, vec3& btn, vec3& nrm, float& mm);
 }
 
 #endif
